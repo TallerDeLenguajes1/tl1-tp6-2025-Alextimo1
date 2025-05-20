a4269c09c5631f7c924314178306c28e476b3da9
Alejandro <alextimo2003@gmail.com>
2025-05-20 15:52:53 -0300

    Realizo ejercicio 1

--- Program.cs
+++ Program.cs
@@ -1,3 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Ingrese un numero mayor que cero: ");
 
+string entrada = Console.ReadLine();
+
+bool esNumero = int.TryParse(entrada, out int numero);
+
+if (esNumero && numero > 0)
+{
+    string numeroInvertidoTexto = "";
+
+    for (int i = entrada.Length - 1; i >= 0; i--)
+    {
+        numeroInvertidoTexto += entrada[i];
+    }
+    Console.WriteLine("Numero invertido: " + numeroInvertidoTexto);
+}
+else
+{
+    Console.WriteLine("La entrada es un numero no valido");
+}
+
+Console.ReadLine();
