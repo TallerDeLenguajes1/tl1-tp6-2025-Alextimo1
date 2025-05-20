06ecda5bb171077af56d3d629ed352b0091a2e9c
Alejandro <alextimo2003@gmail.com>
2025-05-20 16:22:37 -0300

    Realizando ejercicio 2

--- Program.cs
+++ Program.cs
@@ -1,24 +1,62 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Ingrese un numero mayor que cero: ");
+bool continuar = true;
 
-string entrada = Console.ReadLine();
-
-bool esNumero = int.TryParse(entrada, out int numero);
-
-if (esNumero && numero > 0)
+while (continuar)
 {
-    string numeroInvertidoTexto = "";
+    Console.WriteLine("\n --- Calculadora Basica ---");
+    Console.WriteLine("1. Suma");
+    Console.WriteLine("2. Resta");
+    Console.WriteLine("3. Multiplicacion");
+    Console.WriteLine("4. Division");
+    Console.WriteLine("Seleccione una opcion: ");
 
-    for (int i = entrada.Length - 1; i >= 0; i--)
+    string opcion = Console.ReadLine();
+
+    Console.WriteLine("Ingrese el primer numero");
+    bool esNumero1 = double.TryParse(Console.ReadLine(), out double num1);
+
+    Console.WriteLine("Ingrese el segundo numero");
+    bool esNumero2 = double.TryParse(Console.ReadLine(), out double num2);
+
+    if (!esNumero1 || !esNumero2)
     {
-        numeroInvertidoTexto += entrada[i];
+        Console.WriteLine("Error: uno o ambos numeros no son validos");
     }
-    Console.WriteLine("Numero invertido: " + numeroInvertidoTexto);
-}
-else
-{
-    Console.WriteLine("La entrada es un numero no valido");
+    else
+    {
+        switch (opcion)
+        {
+            case "1":
+                Console.WriteLine($"Resultado: {num1} + {num2} = {num1 + num2}");
+                break;
+            case "2":
+                Console.WriteLine($"Resultado: {num1} - {num2} = {num1 - num2}");
+                break;
+            case "3":
+                Console.WriteLine($"Resultado: {num1} * {num2} = {num1 * num2}");
+                break;
+            case "4":
+                if (num2 != 0)
+                {
+                    Console.WriteLine($"Resultado: {num1} / {num2} = {num1 / num2}");
+                }
+                else
+                {
+                    Console.WriteLine("Error: no se puede dividir por cero");
+                }
+                break;
+            default:
+                Console.WriteLine("Opcion no valida. Ingrese (1-4)");
+                break;
+        }
+    }
+    Console.WriteLine("\nDesea realizar otra operacion? (s/n)");
+    string respuesta = Console.ReadLine();
+    if (respuesta != "s" && respuesta != "S")
+    {
+        continuar = false;
+        Console.WriteLine("Saliendo de calculadora...");
+    }
 }
 
 Console.ReadLine();
